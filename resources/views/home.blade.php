12eea54ec351510ef020fda01cf3e60784d4446a
Marc Enzo <emarc237@gmail.com>
2020-04-01 21:16:12 +0100

    style(website): home stylesheet
    Added css for the homepage.
    Note that the products section wasn't yet designed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,58 +9,74 @@
     <link rel="stylesheet" href="/assets/css/normalize.css">
     <link rel="stylesheet" href="/assets/css/ivy.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.css">
+
+    <script src="/assets/js/typed.min.js"></script>
+    <script src="/assets/js/scrollreveal.min.js"></script>
 </head>
 <body>
     <header>
         <div class="row navbar">
             <div class="nine columns">
-                <span>Vous recherchez du travail?</span> <a href="#">Nous Recrutons!</a>
+                <span>Vous recherchez du travail? <a href="#">Nous Recrutons!</a></span>
             </div>
             <div class="three columns">
-                <a href="#">MENU</a>
-                <img src="/assets/img/menu.svg" alt="menu">
+                <a class="menu_button" href="#">
+                    MENU
+                    <img src="/assets/img/menu.svg" alt="menu">
+                </a>
                 <a class="button button-primary" href="#">GET QUOTE</a>
             </div>
         </div>
         <div class="row content">
             <div class="eleven columns">
                 <h5>Mveng Engineering</h5>
-                <h1 style="font-family: 'Gilroy Bold', sans-serif">Le succès d'un produit dépends de<br>la satisfaction de l'utilisateur</h1>
-                <input type="text" placeholder="Your email address here">
-                <a class="button button-primary" href="#">Send</a>
+                <h1>Le succès d'un produit dépends de<br>la satisfaction de l'utilisateur</h1>
+                <div class="row email_container">
+                    <input type="email" name="email" placeholder="Votre adresse email ici">
+                    <a class="button button-primary" href="#">
+                        <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
             </div>
             <div class="one column">
-                <ul>
+                <ul class="socials">
                     <li>
-                        <a href="#"><img src="/assets/img/facebook.svg" alt="facebook"></a>
+                        <a href="#">
+                            <i class="fa fa-facebook"></i>
+                        </a>
                     </li>
                     <li>
-                        <a href="#"><img src="/assets/img/twitter.svg" alt="twitter"></a>
+                        <a href="#">
+                            <i class="fa fa-twitter"></i>
+                        </a>
                     </li>
                     <li>
-                        <a href="#"><img src="/assets/img/linkedin.svg" alt="linkedin"></a>
+                        <a href="#">
+                            <i class="fa fa-linkedin"></i>
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
     </header>
     <section id="about">
-        <!-- Remplacer les h1 par une citation du PDG -->
-        <h1>Mveng Engineering est un studio créatif spécialisé en</h1>
-        <h1 id="services_title">branding & digital.</h1>
-        <a href="#" class="button button-primary">À propos de Mveng Engineering</a>
+        <h1>“Le succès d'un produit dépends de la satisfaction des utilisateurs, chez Mveng Engineering  nous garantissons le mariage entre vos produits et vos clients.“</h1>
+        <span class="author"><b>J. D. Mveng</b> - PDG chez Mveng Engineering</span>
     </section>
     <section id="services">
-        <h3>Des services adaptés à vos besoins</h3>
-        <hr>
         <div class="row">
-            <div class="six columns">
-                <h1>Experts dans tous les aspects du cycle de vie de votre produit</h1>
-                <p>Que vous soyez une énorme entreprise ou une start-up qui vient de démarrer - nous mettons notre créativité, nos talents et nos compétences en chantier pour votre succès.</p>
+            <div class="six columns right">
+                <div class="parallax" data-rellax-speed="-6">
+                    <h3>Des services adaptés à vos besoins</h3>
+                    <hr>
+                    <h1>Experts dans tous les aspects du cycle de vie de votre produit</h1>
+                    <p>Que vous soyez une énorme entreprise ou une start-up qui vient de démarrer - nous mettons notre créativité, nos talents et nos compétences en chantier pour votre succès.</p>
+                </div>
             </div>
             <div class="six columns">
-                <div class="row services">
-                    <article class="service">
+                <div class="row services" data-rellax-speed="4">
+                    <article class="service" id="branding">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">01</span>
@@ -69,7 +85,7 @@
                             <h1>Br</h1>
                         </div>
                     </article>
-                    <article class="service">
+                    <article class="service" id="development">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">02</span>
@@ -78,7 +94,7 @@
                             <h1>Dl</h1>
                         </div>
                     </article>
-                    <article class="service">
+                    <article class="service" id="design">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">03</span>
@@ -87,7 +103,7 @@
                             <h1>Dg</h1>
                         </div>
                     </article>
-                    <article class="service">
+                    <article class="service" id="solutions">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">04</span>
@@ -96,7 +112,7 @@
                             <h1>Si</h1>
                         </div>
                     </article>
-                    <article class="service">
+                    <article class="service" id="maintenance">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">05</span>
@@ -105,7 +121,7 @@
                             <h1>Mi</h1>
                         </div>
                     </article>
-                    <article class="service">
+                    <article class="service" id="video">
                         <div class="service_cover">
                             <div class="service_content">
                                 <span class="number">06</span>
@@ -119,18 +135,18 @@
         </div>
     </section>
     <section id="testimonials">
-        <h1>"La meilleure agence avec laquelle nous avons eu à travailler. Mveng Engineering comprends notre produit et est capable d'ajouter des fonctionnalités qui nous font grandir sur le marché."</h1>
-        <img src="/assets/img/testimonial.svg" alt="testimonial" class="testimonial_logo"><br>
-        <p><b>Marc Enzo</b> / Directeur de l'innovation chez Pocket Inc</p>
+        <h1>“La meilleure agence avec laquelle nous avons eu à travailler. Mveng Engineering comprends notre produit et est capable d'ajouter des fonctionnalités qui nous font grandir sur le marché.“</h1>
+        <i class="fa fa-github"></i><br>
+        <span><b>Marc Enzo</b> / Directeur de l'innovation chez Github Cameroon</span>
         <p class="our_testimonial">Mveng Engineering crée des applications qui aident les gens à évoluer différemment. Partagez votre défi avec notre équipe et nous travaillerons avec vous pour livrer un produit révolutionnaire.</p>
     </section>
     <section id="products">
-        <div class="row">
+        <div class="row content">
             <div class="one column">
-                <a href="#">Prev</a>
+                <a class="prev_btn" href="#">Prev</a>
             </div>
             <div class="ten columns products">
-                <div class="row product">
+                <div class="row product active">
                     <div class="three columns">
                         <span class="product_number">01</span>
                         <h2 class="product_title">Decentralized Platform App</h2>
@@ -158,16 +174,18 @@
                 </div>
             </div>
             <div class="one column">
-                <a href="#">Next</a>
+                <a class="next_btn" href="#">Next</a>
             </div>
         </div>
     </section>
     <section id="end">
-        <h1>On a 01 an aujourd'hui et c'est le début d'une belle aventure.</h1>
+        <h1 class="parallax" data-rellax-speed="0">On a 01 an aujourd'hui mais on reste chez nous. <b>#ResteChezToi</b></h1>
     </section>
     <footer>
         <div class="row contact_header">
-            <a href="#">Contactez nous</a>
+            <a href="#">
+                <h2>Contactez nous</h2>
+            </a>
         </div>
 
         <div class="row footer_content">
@@ -183,7 +201,7 @@
                     <li>KRS: 0000745671</li>
                 </ul>
             </div>
-            <div class="six columns">
+            <div class="six columns menu">
                 <ul>
                     <li><a href="#">Equipe</a></li>
                     <li><a href="#">Blog</a></li>
@@ -198,6 +216,41 @@
                 </ul>
             </div>
         </div>
+
+        <div class="row footer_footer">
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-github"></i></a>
+            <a href="#"><i class="fa fa-dribbble"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
     </footer>
+
+<script src="/assets/js/underscore.js"></script>
+<script src="/assets/js/jquery.js"></script>
+<script src="/assets/js/rellax.js"></script>
+<script src="/assets/js/cursor.js"></script>
+<script>
+    // Rounded cursor 02b250
+    const cursor = cursorDot({
+        borderWidth: 2,
+        diameter: 50,
+        easing: 4,
+        borderColor: "rgba(0, 0, 0, 0)"
+    });
+
+    // Parallax scrolling
+    const rellax = Rellax('.parallax', {
+        breakpoints: [576, 768, 1201]
+    });
+
+    cursor.over('footer', {
+        background: "transparent",
+        scale: 1.1,
+        borderColor: "#fff"
+    });
+
+</script>
 </body>
 </html>

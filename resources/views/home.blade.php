03c8236263417eb0d7054ffe6a5f662697120872
Marc Enzo <emarc237@gmail.com>
2020-04-01 05:32:02 +0100

    style(website): website architecture
    Created the website architecture without styles.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mveng Engineering - Agence de Création de Site internet à Yaoundé, Conception, Conseil</title>
+    <link rel="stylesheet" href="/assets/css/normalize.css">
+    <link rel="stylesheet" href="/assets/css/ivy.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="row navbar">
+            <div class="nine columns">
+                <span>Vous recherchez du travail?</span> <a href="#">Nous Recrutons!</a>
+            </div>
+            <div class="three columns">
+                <a href="#">MENU</a>
+                <img src="/assets/img/menu.svg" alt="menu">
+                <a class="button button-primary" href="#">GET QUOTE</a>
+            </div>
+        </div>
+        <div class="row content">
+            <div class="eleven columns">
+                <h5>Mveng Engineering</h5>
+                <h1 style="font-family: 'Gilroy Bold', sans-serif">Le succès d'un produit dépends de<br>la satisfaction de l'utilisateur</h1>
+                <input type="text" placeholder="Your email address here">
+                <a class="button button-primary" href="#">Send</a>
+            </div>
+            <div class="one column">
+                <ul>
+                    <li>
+                        <a href="#"><img src="/assets/img/facebook.svg" alt="facebook"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="/assets/img/twitter.svg" alt="twitter"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="/assets/img/linkedin.svg" alt="linkedin"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section id="about">
+        <!-- Remplacer les h1 par une citation du PDG -->
+        <h1>Mveng Engineering est un studio créatif spécialisé en</h1>
+        <h1 id="services_title">branding & digital.</h1>
+        <a href="#" class="button button-primary">À propos de Mveng Engineering</a>
+    </section>
+    <section id="services">
+        <h3>Des services adaptés à vos besoins</h3>
+        <hr>
+        <div class="row">
+            <div class="six columns">
+                <h1>Experts dans tous les aspects du cycle de vie de votre produit</h1>
+                <p>Que vous soyez une énorme entreprise ou une start-up qui vient de démarrer - nous mettons notre créativité, nos talents et nos compétences en chantier pour votre succès.</p>
+            </div>
+            <div class="six columns">
+                <div class="row services">
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">01</span>
+                                <h4 class="service_title">Branding</h4>
+                            </div>
+                            <h1>Br</h1>
+                        </div>
+                    </article>
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">02</span>
+                                <h4 class="service_title">Développement Logiciel</h4>
+                            </div>
+                            <h1>Dl</h1>
+                        </div>
+                    </article>
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">03</span>
+                                <h4 class="service_title">Design Graphique</h4>
+                            </div>
+                            <h1>Dg</h1>
+                        </div>
+                    </article>
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">04</span>
+                                <h4 class="service_title">Solutions Informatiques</h4>
+                            </div>
+                            <h1>Si</h1>
+                        </div>
+                    </article>
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">05</span>
+                                <h4 class="service_title">Assistance & Maintenance Informatique</h4>
+                            </div>
+                            <h1>Mi</h1>
+                        </div>
+                    </article>
+                    <article class="service">
+                        <div class="service_cover">
+                            <div class="service_content">
+                                <span class="number">06</span>
+                                <h4 class="service_title">Vidéosurveillance</h4>
+                            </div>
+                            <h1>Vs</h1>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="testimonials">
+        <h1>"La meilleure agence avec laquelle nous avons eu à travailler. Mveng Engineering comprends notre produit et est capable d'ajouter des fonctionnalités qui nous font grandir sur le marché."</h1>
+        <img src="/assets/img/testimonial.svg" alt="testimonial" class="testimonial_logo"><br>
+        <p><b>Marc Enzo</b> / Directeur de l'innovation chez Pocket Inc</p>
+        <p class="our_testimonial">Mveng Engineering crée des applications qui aident les gens à évoluer différemment. Partagez votre défi avec notre équipe et nous travaillerons avec vous pour livrer un produit révolutionnaire.</p>
+    </section>
+    <section id="products">
+        <div class="row">
+            <div class="one column">
+                <a href="#">Prev</a>
+            </div>
+            <div class="ten columns products">
+                <div class="row product">
+                    <div class="three columns">
+                        <span class="product_number">01</span>
+                        <h2 class="product_title">Decentralized Platform App</h2>
+                        <span class="product_type">Blockchain Platform</span>
+                    </div>
+                    <div class="six columns">
+                        <img src="/assets/img/product1.png" alt="product_1" class="product_img">
+                    </div>
+                    <div class="three columns">
+                        <a href="#" class="button button_primary">Voir le projet</a>
+                    </div>
+                </div>
+                <div class="row product">
+                    <div class="three columns">
+                        <span class="product_number">02</span>
+                        <h2 class="product_title">Creditor AI</h2>
+                        <span class="product_type">Blockchain Platform</span>
+                    </div>
+                    <div class="six columns">
+                        <img src="/assets/img/product2.png" alt="product_2" class="product_img">
+                    </div>
+                    <div class="three columns">
+                        <a href="#" class="button button_primary">Voir le projet</a>
+                    </div>
+                </div>
+            </div>
+            <div class="one column">
+                <a href="#">Next</a>
+            </div>
+        </div>
+    </section>
+    <section id="end">
+        <h1>On a 01 an aujourd'hui et c'est le début d'une belle aventure.</h1>
+    </section>
+    <footer>
+        <div class="row contact_header">
+            <a href="#">Contactez nous</a>
+        </div>
+
+        <div class="row footer_content">
+            <div class="six columns">
+                <ul>
+                    <li>Mveng Engineering S.A.</li>
+                    <li>ul. Male Garbary 9</li>
+                    <li>61-756 Odza, Yaoundé</li>
+                </ul>
+                <ul>
+                    <li>VAT-ID: PL7781454968</li>
+                    <li>REGON: 300826280</li>
+                    <li>KRS: 0000745671</li>
+                </ul>
+            </div>
+            <div class="six columns">
+                <ul>
+                    <li><a href="#">Equipe</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Conditions d'utilisation</a></li>
+                    <li><a href="#">Livre blanc</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Vie privée</a></li>
+                    <li><a href="#">Notre histoire</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
